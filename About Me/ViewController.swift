77cbca0e0273d2579fc1478ff3138f933691529c
Lambda_School_Loaner_18 <lambda_school_loaner_18@lambda-school-loaner-18s-macbook-pro.local>
2018-09-11 21:31:59 -0400

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Lambda_School_Loaner_18 on 9/11/18.
+//  Copyright © 2018 Lambda_School_Loaner_18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
